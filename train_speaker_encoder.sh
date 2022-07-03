24174d6d05f72baf7728ce28a1e284c93ed596af
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-07-03 00:44:19 -0700

    upload train_speaker_encoder.sh

---
+++ train_speaker_encoder.sh
@@ -0,0 +1,5 @@
+python train.py \
+--checkpoint_path speaker_checkpoint \
+--training_file speaker_data/training.txt \
+--validation_file speaker_data/validation.txt \
+--config config/config_sr.json \
