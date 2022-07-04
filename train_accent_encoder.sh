e9f73406ff7d1f4821fd62576f15b4af6953f8ec
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-07-03 19:17:44 -0700

    add accent encoder sh

---
+++ train_accent_encoder.sh
@@ -0,0 +1,5 @@
+python train.py \
+--checkpoint_path checkpoint/checkpoint_06_27_2022 \
+--training_file data/training.txt \
+--validation_file data/validation.txt \
+--config config/config_ac.json \
