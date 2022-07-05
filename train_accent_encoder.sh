79e5bf898e5c8d1bcf8e11f57e4425f3a05e3041
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-07-05 16:10:54 -0700

    update train_accent_encoder.sh

--- train_accent_encoder.sh
+++ train_accent_encoder.sh
@@ -1,5 +1,5 @@
 nohup python train.py \
---checkpoint_path checkpoint/checkpoint_06_27_2022 \
+--checkpoint_path checkpoint/checkpoint_07052022\
 --training_file data/training.txt \
 --validation_file data/validation.txt \
 --config config/config_ac.json \
